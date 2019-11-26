05dce6b0a7ebe97b5350e065c3c59c300728c028
Adem AKKUŞ <32845253+ademakkus@users.noreply.github.com>
2019-11-26 15:54:30 +0300

    Stopwatch
    stopwatch

---
+++ CalismaZamaniBulma/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static  System.Console;
+using  static System.ConsoleColor;
+namespace CalismaZamaniBulma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ForegroundColor = Red;
+            Console.WriteLine("ÇALIŞMA ZAMANI TEST ETME");
+            ForegroundColor = DarkGreen;
+            var sw = new Stopwatch();
+            var tmpString="";
+            sw.Start();
+            for (int i = 0; i < 10000; i++)
+            {
+                tmpString += i.ToString();
+              //  WriteLine(tmpString);
+            }
+            sw.Stop();
+            Console.WriteLine("Execution time :"+sw.ElapsedMilliseconds);
+            var sw2=new Stopwatch();
+            sw2.Start();
+            var sb=new StringBuilder();
+            for (int i = 0; i < 10000; i++)
+            {
+                sb.Append(i.ToString());
+                //  WriteLine(tmpString);
+            }
+            sw2.Stop();
+            ForegroundColor = DarkYellow;
+            Console.WriteLine("StringbuilderExecution time :" + sw2.ElapsedMilliseconds);
+            ReadKey();
+        }
+    }
+}
---
+++ ParamsKeyword/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using  static System.Console;
+using static System.ConsoleColor;
+namespace ParamsKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double average1, average2;
+            ForegroundColor = DarkGreen;
+            average1 = CalculateAverage(2.54, 4, 123, 78);
+            WriteLine("First average : {0}",average1);
+            ForegroundColor = DarkRed;
+            average2 = CalculateAverage(3, 45, 66, 90, 8);
+            WriteLine("Second average : {0}", average2);
+            ReadKey();
+        }
+
+        static double CalculateAverage(params double[] values)
+        {
+
+            double sum = 0;
+            ForegroundColor = DarkYellow;
+            WriteLine("You sent {0} doubles.",values.Length);
+            if (values.Length == 0)
+                return sum;
+            for (int i = 0; i < values.Length; i++)
+                sum += values[i];
+            return (sum / values.Length);
+        }
+        
+    }
+}
